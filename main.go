f7484ef0871a5d78a1e1eaaedbf66d885647f695
kiwsan <kiwsanthia@gmail.com>
2019-06-26 14:49:36 +0700

    update command

--- main.go
+++ main.go
@@ -1,23 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"github.com/urfave/cli"
+  "log"
+  "os"
+  "sort"
+
+  "github.com/urfave/cli"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "greet"
-	app.Usage = "fight the loneliness!"
-	app.Action = func(c *cli.Context) error {
-		fmt.Println("Hello friend!")
-		return nil
-	}
+  app := cli.NewApp()
 
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
+  app.Flags = []cli.Flag {
+    cli.StringFlag{
+      Name: "lang, l",
+      Value: "english",
+      Usage: "Language for the greeting",
+    },
+    cli.StringFlag{
+      Name: "config, c",
+      Usage: "Load configuration from `FILE`",
+    },
+  }
+
+  app.Commands = []cli.Command{
+    {
+      Name:    "complete",
+      Aliases: []string{"c"},
+      Usage:   "complete a task on the list",
+      Action:  func(c *cli.Context) error {
+        return nil
+      },
+    },
+    {
+      Name:    "add",
+      Aliases: []string{"a"},
+      Usage:   "add a task to the list",
+      Action:  func(c *cli.Context) error {
+        return nil
+      },
+    },
+  }
+
+  sort.Sort(cli.FlagsByName(app.Flags))
+  sort.Sort(cli.CommandsByName(app.Commands))
+
+  err := app.Run(os.Args)
+  if err != nil {
+    log.Fatal(err)
+  }
 }
